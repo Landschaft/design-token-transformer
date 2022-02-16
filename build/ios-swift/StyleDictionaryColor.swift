cae6463cabc597135984b9f5fae6597dece64fbd
Landschaft <Landschaft@users.noreply.github.com>
2022-02-16 16:13:37 +0000

    Tokens updated

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,18 +4,34 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Wed, 16 Feb 2022 16:13:36 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
-    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.10196078431372549)
-    public static let colorsRefBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1)
-    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
-    public static let darkBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let lightBackground = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let boxshadowCardColor = rgba(190, 195, 197, 1)
+    public static let boxshadowFabColor = rgba(190, 195, 197, 1)
+    public static let boxshadowFocusColor = rgba(0, 122, 255, 0.5)
+    public static let brandActive = UIColor(red: 0.898, green: 0.149, blue: 0.122, alpha: 1)
+    public static let grayBase = UIColor(red: 0.502, green: 0.545, blue: 0.561, alpha: 1)
+    public static let grayDark = UIColor(red: 0.251, green: 0.275, blue: 0.282, alpha: 1)
+    public static let grayHighDark = UIColor(red: 0.376, green: 0.408, blue: 0.420, alpha: 1)
+    public static let grayHighLight = UIColor(red: 0.871, green: 0.882, blue: 0.886, alpha: 1)
+    public static let grayLight = UIColor(red: 0.745, green: 0.765, blue: 0.773, alpha: 1)
+    public static let grayLowDark = UIColor(red: 0.125, green: 0.137, blue: 0.141, alpha: 1)
+    public static let grayLowLight = UIColor(red: 0.624, green: 0.655, blue: 0.667, alpha: 1)
+    public static let grayWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let primaryActive = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let secondaryActive = UIColor(red: 0.122, green: 0.439, blue: 0.600, alpha: 1)
+    public static let secondaryHover = UIColor(red: 0.129, green: 0.506, blue: 0.675, alpha: 1)
+    public static let secondaryPressed = UIColor(red: 0.082, green: 0.322, blue: 0.467, alpha: 1)
+    public static let statusColoursError = UIColor(red: 1.000, green: 0.000, blue: 0.322, alpha: 1)
+    public static let statusColoursErrorLight = UIColor(red: 1.000, green: 0.808, blue: 0.878, alpha: 1)
+    public static let statusColoursInfo = UIColor(red: 0.000, green: 0.651, blue: 0.996, alpha: 1)
+    public static let statusColoursInfoLight = UIColor(red: 0.792, green: 0.941, blue: 1.000, alpha: 1)
+    public static let statusColoursSuccess = UIColor(red: 0.000, green: 0.780, blue: 0.000, alpha: 1)
+    public static let statusColoursSuccessLight = UIColor(red: 0.784, green: 0.969, blue: 0.808, alpha: 1)
+    public static let tertiaryActionButton = UIColor(red: 0.000, green: 0.749, blue: 0.749, alpha: 1)
+    public static let tertiaryHoverShade = UIColor(red: 0.980, green: 0.980, blue: 0.980, alpha: 1)
 }
--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Wed, 16 Feb 2022 16:13:36 GMT
 
 
 #import "StyleDictionaryColor.h"
@@ -21,14 +21,30 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
-[UIColor colorWithRed:0.251f green:1.000f blue:0.729f alpha:1.000f],
-[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:0.102f],
-[UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
-[UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
-[UIColor colorWithRed:0.251f green:0.875f blue:0.314f alpha:1.000f],
-[UIColor colorWithRed:0.204f green:0.337f blue:0.686f alpha:1.000f],
+[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f],
+[UIColor colorWithRed:0.122f green:0.439f blue:0.600f alpha:1.000f],
+[UIColor colorWithRed:0.129f green:0.506f blue:0.675f alpha:1.000f],
+[UIColor colorWithRed:0.082f green:0.322f blue:0.467f alpha:1.000f],
+[UIColor colorWithRed:0.980f green:0.980f blue:0.980f alpha:1.000f],
+[UIColor colorWithRed:0.000f green:0.749f blue:0.749f alpha:1.000f],
+[UIColor colorWithRed:0.898f green:0.149f blue:0.122f alpha:1.000f],
 [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f],
-[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f]
+[UIColor colorWithRed:0.871f green:0.882f blue:0.886f alpha:1.000f],
+[UIColor colorWithRed:0.745f green:0.765f blue:0.773f alpha:1.000f],
+[UIColor colorWithRed:0.624f green:0.655f blue:0.667f alpha:1.000f],
+[UIColor colorWithRed:0.502f green:0.545f blue:0.561f alpha:1.000f],
+[UIColor colorWithRed:0.376f green:0.408f blue:0.420f alpha:1.000f],
+[UIColor colorWithRed:0.251f green:0.275f blue:0.282f alpha:1.000f],
+[UIColor colorWithRed:0.125f green:0.137f blue:0.141f alpha:1.000f],
+[UIColor colorWithRed:1.000f green:0.000f blue:0.322f alpha:1.000f],
+[UIColor colorWithRed:1.000f green:0.808f blue:0.878f alpha:1.000f],
+[UIColor colorWithRed:0.000f green:0.651f blue:0.996f alpha:1.000f],
+[UIColor colorWithRed:0.792f green:0.941f blue:1.000f alpha:1.000f],
+[UIColor colorWithRed:0.000f green:0.780f blue:0.000f alpha:1.000f],
+[UIColor colorWithRed:0.784f green:0.969f blue:0.808f alpha:1.000f],
+rgba(0, 122, 255, 0.5),
+rgba(190, 195, 197, 1),
+rgba(190, 195, 197, 1)
     ];
   });
 
